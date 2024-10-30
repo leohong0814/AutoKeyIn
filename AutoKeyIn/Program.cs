6d327383edfcba6674d8cc1c0aec8fca0e08eb77
leohong0814 <leohong0814@gmail.com>
2024-10-30 14:44:49 +0800

    Implement basic function

--- AutoKeyIn/Program.cs
+++ AutoKeyIn/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace AutoKeyIn
 {
@@ -10,6 +13,43 @@ namespace AutoKeyIn
     {
         static void Main(string[] args)
         {
+            string windowName = Properties.Settings.Default.WindowName;
+            int interval = Properties.Settings.Default.ClickInterval;
+            string key = Properties.Settings.Default.KeyinKey;
+
+            if (string.IsNullOrEmpty(windowName) )
+            {
+                Console.WriteLine("Enter window name");
+                windowName = Console.ReadLine();
+                Console.WriteLine("Enter interval time(s)");
+                interval = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter whitch key nead to auto key in");
+                key = Console.ReadLine();
+                Properties.Settings.Default.WindowName = windowName;
+                Properties.Settings.Default.ClickInterval = interval;
+                Properties.Settings.Default.KeyinKey = key;
+                Properties.Settings.Default.Save();
+            }
+            
+            WindowControl windowControl = new WindowControl();
+            CancellationTokenSource ctx = new CancellationTokenSource();
+            if (!windowControl.WaitWindowOnline(ctx.Token, windowName))
+            {
+                Console.WriteLine($"Can't find {windowName} window");
+                return;
+            }
+            Console.WriteLine("Press F5 to Start");
+            while (!ctx.IsCancellationRequested && Console.ReadKey().Key != ConsoleKey.F5)
+            {
+                Console.WriteLine("Press F5 to Start");
+            }
+            Thread.Sleep(500);
+            while (!ctx.IsCancellationRequested)
+            {
+                Thread.Sleep(interval*1000);
+                SendKeys.Send(key);
+            }
+            
         }
     }
 }
---
+++ AutoKeyIn/WindowControl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoKeyIn
+{
+    public class WindowControl
+    {
+        // Get a handle to an application window.
+        [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        // Activate an application window.
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        public bool WaitWindowOnline(CancellationToken cancelByUser, string windowName, int retryTimes = -1)
+        {
+            IntPtr calculatorHandle = IntPtr.Zero;
+            int retry = 0;
+            while (calculatorHandle == IntPtr.Zero)
+            {
+                cancelByUser.ThrowIfCancellationRequested();
+                calculatorHandle = FindWindow(null, windowName);
+                if (retry == retryTimes)
+                {
+                    return false;
+                }
+                Thread.Sleep(500);
+                retry++;
+            }
+            SetForegroundWindow(calculatorHandle);
+            return true;
+        }
+    }
+}
